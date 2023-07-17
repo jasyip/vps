3b0457b4527a2694000a008a1159895a2c0a8372
Jason Yip <general@jasonyip1.anonaddy.me>
2023-07-16 19:46:58 -0700

    [UPDATE]
    - Remove `compose.py` production argument
    - Add debug statements to `compose.py`

--- mount/compose.py
+++ mount/compose.py
@@ -12,10 +12,10 @@ import signal
 import subprocess
 import time
 from argparse import ArgumentParser
-from collections.abc import Iterable, Sequence
+from collections.abc import Iterable
 from pathlib import Path, PurePath
 from string import Template
-from typing import Any, Final, Optional
+from typing import Final, Optional
 
 COMPOSE: Final[str] = "podman-compose"
 SET_METADATA_CMD: Final[tuple[str, ...]] = ("/opt/set-metadata", ".", "server")
@@ -24,8 +24,12 @@ subcommand_args: Final[list[str]] = []
 
 own_parser: Final = ArgumentParser()
 own_parser.add_argument("-d", "--debug", action="store_true")
-own_parser.add_argument("-p", "--production", action="store_true")
-own_parser.add_argument("--less-opts", default="", type=shlex.split)
+own_parser.add_argument(
+    "--less-opts",
+    default="",
+    type=shlex.split,
+    help="options to pass to 'less', which may be invoked depending on provided podman-compose subcommand",
+)
 
 _logger: Final = logging.getLogger(__name__)
 
@@ -50,25 +54,25 @@ logging.basicConfig(
     stream=sys.stderr, level=logging.DEBUG if own_args.debug else logging.INFO
 )
 
+_logger.debug(f"Parsed arguments for ownself: {own_args}")
+
+_logger.debug(f"Initial {main_args=}")
+_logger.debug(f"Initial {subcommand_args=}")
+
 os.chdir(PurePath(__file__).parent)
 
-stage_file: Final[Path] = Path(
-    "production.yaml" if own_args.production else "development.yaml"
-)
-if not stage_file.is_file():
-    raise OSError(f"{stage_file} is not a file")
-main_args.extend(("-f", str(stage_file)))
-
-
 subcommand: Final[str] = sys.argv[_subcommand_ind]
+_logger.debug(f"{subcommand=}")
 
 
 def command(args: Optional[Iterable[str]] = None) -> tuple[str, ...]:
-    return (
+    output: Final = (
         COMPOSE,
         *main_args,
         *((subcommand, *subcommand_args) if args is None else args),
     )
+    _logger.debug(f"Will execute: {output}")
+    return output
 
 
 match subcommand:
@@ -86,6 +90,7 @@ match subcommand:
             tmp_dir = Path("~", ".local", "state", "tmp")
             tmp_dir.mkdir(parents=True, exist_ok=True)
         env_file: Final[Path] = tmp_dir / "compose.env"
+        _logger.debug(f"{env_file=}")
 
         ps_proc: Final = subprocess.run(
             command(("--podman-args", "--all --format json", "ps")),
@@ -98,6 +103,7 @@ match subcommand:
         if all(
             container["State"] in {"dead", "exited"} for container in ps_json_output
         ):
+            _logger.debug("Generating environment file from template")
             env_template: Final = Template(Path(".env").read_text())
             env_file.write_text(
                 env_template.safe_substitute(
@@ -108,19 +114,27 @@ match subcommand:
         main_args.extend(("--env-file", str(env_file)))
         subcommand_args.append("-d")
 
+_logger.debug(f"default main_args based on subcommand: {main_args}")
+_logger.debug(f"default subcommand_args based on subcommand: {subcommand_args}")
+
 main_args.extend(additional_main_args)
 subcommand_args.extend(sys.argv[_subcommand_ind + 1 :])
 
+_logger.debug(f"{main_args=}")
+_logger.debug(f"{subcommand_args=}")
+
+_logger.debug(f"Executing {SET_METADATA_CMD}")
 subprocess.run(SET_METADATA_CMD, check=True)
 
 final_command: Final[tuple[str, ...]] = command()
 _logger.info(f"Executing: {final_command}")
 match subcommand:
     case "logs":
-        pager_command: Final[Sequence[str]] = (
+        pager_command: Final[tuple[str, ...]] = (
             "/usr/bin/less",
             *own_args.less_opts,
         )
+        _logger.debug(f"{pager_command=}")
         with subprocess.Popen(
             final_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
         ) as compose_proc:
