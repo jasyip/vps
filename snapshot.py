a2f2adb1a1f7eedfd8620ad2c7693795a7648a94
Jason Yip <general@jasonyip.slmail.me>
2024-08-10 15:14:17 -0700

    refactor: correct output message of `snapshot.py`

--- snapshot.py
+++ snapshot.py
@@ -189,4 +189,4 @@ if __name__ == "__main__":
     if output is None:
         _logger.info("Done!")
     else:
-        _logger.info(f"New raw image: '{output}'")
+        _logger.info(f"New image: '{output}'")
