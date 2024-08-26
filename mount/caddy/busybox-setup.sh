912546f77a392c9907acc133493aa2e273205ab2
Jason Yip <general@jasonyip.slmail.me>
2024-08-25 18:09:53 -0700

    style(dockerfile): increase debugging output/shell command consistency
    - prefer setting `-eCuxo pipefail` in shells
    - remove `-q` option when using `wget`
    - use `tee` over `pee`

--- mount/caddy/busybox-setup.sh
+++ mount/caddy/busybox-setup.sh
@@ -3,13 +3,11 @@
 # SPDX-FileCopyrightText: 2024 Jason Yip <general@jasonyip.slmail.me>
 # SPDX-License-Identifier: GPL-2.0-only
 
-set -euo pipefail
-
 gpg --recv-key C9E9416F76E610DBD09D040F47B70C55ACC9965B
 
 URL="https://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2"
-wget -qO - "$URL" | pee 'tar -xjof - --strip-components 1' \
-                        "gpg --verify <(wget -qO - '${URL}.sig') -" cat |
+wget -qO - "$URL" | tee >(tar -xjof - --strip-components 1) \
+                        >(gpg --verify <(wget -qO - "${URL}.sig") -) |
                         sha256sum -c <(wget -qO - "${URL}.sha256" | awk '{print $1 " -"}')
 
 
